db604e847c90b9148ae471f2371b420391d38b80
KOSMO <nuremirik@gmail.com>
2024-09-17 10:26:26 +0600

    change navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Css from './Home.module.css'
 
 function Home() {
   return (
-    <div>
-      
+    <div className={Css.Main}>
+      <div className={Css.MainImage}>
+
+      </div>
+      <div className={Css.News}>
+
+      </div>
+      <div className={Css.Posts}>
+
+      </div>
     </div>
   )
 }
